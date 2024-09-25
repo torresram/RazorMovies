02e72ad30dd721ea11260fe21f8b0bad9d1cd821
torresram <ramirotorres90@yahoo.com.ar>
2024-09-24 21:07:03 -0300

    Razor pages tutorial

---
+++ RazorMovies/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string? Title { get; set; } = string.Empty;
+        [DataType(DataType.Date)]//solo se muestra la fecha, sin la hora;
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; } = string.Empty;
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
---
+++ RazorMovies/SeedData/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorMovies.Data;
+using RazorMovies.Models;
+
+namespace RazorMovies.SeedData
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorMoviesContext(serviceProvider.GetRequiredService<DbContextOptions<RazorMoviesContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("NUll RazorMoviesContext");
+                }
+
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 200,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 999,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 399,
+                        Rating = "R"
+                    }
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
